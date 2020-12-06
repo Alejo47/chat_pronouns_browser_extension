eb4186330e0d3f443eeab803774117966a292c23
Alejo Pereyra <alejo.andres.pereyra@gmail.com>
2020-12-06 20:06:23 -0300

    Mod: Remove console.log + Fix indentation

--- src/ts/api/pronouns.alejo.io.ts
+++ src/ts/api/pronouns.alejo.io.ts
@@ -1,30 +1,29 @@
 import Pronoun, { IPronouns } from "../types/pronouns";
 
 async function get<T = JSON>(endpoint: string): Promise<T> {
-		return await fetch(process.env.BASE_API_URL + endpoint).then(async (res: Response) => {
-			return res.json() as Promise<T>;
-		})
-	}
+	return await fetch(process.env.BASE_API_URL + endpoint).then(async (res: Response) => {
+		return res.json() as Promise<T>;
+	})
+}
 	
 export async function getPronouns(): Promise<IPronouns> {
-		console.log("Getting pronouns");
-		var res: Pronoun[] = await get<Pronoun[]>("pronouns")
-		var p: IPronouns = {};
-		res.forEach((pronoun: Pronoun) => {
-			p[pronoun.name] = pronoun.display;
-		});
-		return p;
-	}
+	var res: Pronoun[] = await get<Pronoun[]>("pronouns")
+	var p: IPronouns = {};
+	res.forEach((pronoun: Pronoun) => {
+		p[pronoun.name] = pronoun.display;
+	});
+	return p;
+}
 
 export async function getUserPronoun(username: string): Promise<string | undefined> {
-		if (username.length < 1) {
-			return;
-		}
-
-		var res = await get<any[]>("users/" + username);
-		var users: any = {};
-		res.forEach((user: any) => {
-			users[user.login] = user.pronoun_id;
-		});
-		return users[username];
+	if (username.length < 1) {
+		return;
 	}
+
+	var res = await get<any[]>("users/" + username);
+	var users: any = {};
+	res.forEach((user: any) => {
+		users[user.login] = user.pronoun_id;
+	});
+	return users[username];
+}
