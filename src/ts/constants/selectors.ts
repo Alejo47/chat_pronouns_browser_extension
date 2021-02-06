a09304e95936a80c49288727da158f7d9439d535
Alejo Pereyra <alejo.andres.pereyra@gmail.com>
2021-02-06 05:34:19 -0300

    Add: FFZ Selectors

--- src/ts/constants/selectors.ts
+++ src/ts/constants/selectors.ts
@@ -1,7 +1,12 @@
 export const ROOT = `#root`
 
 export const LIVE_CHAT_DISPLAY_NAME = `span.chat-author__display-name`
-export const LIVE_CHAT_BADGES = `.chat-line__username-container.tw-inline-block > span:not([class]),.chat-line__message--badges`
+export const LIVE_CHAT_BADGES = `.chat-line__username-container.tw-inline-block > span:not([class])`
 
 export const VOD_CHAT_USERNAME = `[data-test-selector="comment-author-selector"]`
 export const VOD_CHAT_BADGES = `.tw-align-items-start.tw-flex.tw-flex-nowrap > .tw-flex-grow-1 > span:nth-child(1)`
+
+export const FFZ = {
+	LIVE_CHAT_DISPLAY_NAME: `span.chat-line__username`,
+	LIVE_CHAT_BADGES: `span.chat-line__message--badges`,
+}
