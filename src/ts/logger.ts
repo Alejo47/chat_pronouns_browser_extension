0f8071c013ad24dddba0b24c304f08ef01553c23
Alejo Pereyra <alejo.andres.pereyra@gmail.com>
2021-02-07 06:48:28 -0300

    Add: Global Logger

---
+++ src/ts/logger.ts
@@ -0,0 +1,47 @@
+export enum LoggerLevel {
+	NONE,
+	INFO,
+	WARN,
+	DEBUG
+}
+
+export interface ILogger {
+	level: LoggerLevel,
+	outputCommand: (...args: unknown[]) => void;
+}
+
+export class Logger implements ILogger {
+	level: LoggerLevel;
+	outputCommand: (...args: unknown[]) => void;
+
+	constructor(level: LoggerLevel, outputCommand?: (...args: unknown[]) => void) {
+		this.level = level;
+		if (outputCommand !== undefined) {
+			this.outputCommand = outputCommand
+		} else {
+			this.outputCommand = console.log
+		}
+	}
+
+	log(level: LoggerLevel, ...args: unknown[]) {
+		if (this.level === LoggerLevel.NONE) { 
+			return;
+		} else if (this.level >= level) {
+			this.outputCommand(...args)
+		}
+	}
+
+	info(...args: unknown[]) {
+		this.log(LoggerLevel.INFO, ...args)
+	}
+	warn(...args: unknown[]) {
+		this.log(LoggerLevel.WARN, ...args)
+	}
+	debug(...args: unknown[]) {
+		this.log(LoggerLevel.DEBUG, ...args)
+	}
+}
+
+export let BaseLogger = new Logger(LoggerLevel.NONE)
+
+export default BaseLogger
