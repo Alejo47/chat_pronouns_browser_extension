6794fe901aaa95f25e4d4f17f765e2dc319cb7dc
techygrrrl <techygrrrl.streams@gmail.com>
2022-05-28 16:26:10 -0700

    cache user pronouns

--- src/ts/api/pronouns.alejo.io.ts
+++ src/ts/api/pronouns.alejo.io.ts
@@ -1,6 +1,9 @@
 import Pronoun, { IPronouns } from "../types/pronouns";
 import { IUser } from "../types/users";
 
+const userPronounsCache: Record<string,string> = {
+}
+
 async function get<T = JSON>(endpoint: string): Promise<T> {
 	return await fetch(process.env.BASE_API_URL + endpoint).then(async (res: Response) => {
 		return res.json() as Promise<T>;
@@ -21,12 +24,18 @@ export async function getUserPronoun(username: string): Promise<string | undefin
 		return;
 	}
 
+	const cachedPronoun = userPronounsCache[username]
+	if (cachedPronoun) {
+		return cachedPronoun
+	}
+
 	var res = await get<IUser[]>("users/" + username);
 	let match: IUser | undefined = res.find((user: IUser) => {
 		return user.login.toLowerCase() === username.toLowerCase();
 	})
 
 	if (match !== undefined) {
+		userPronounsCache[username] = match.pronoun_id
 		return match.pronoun_id;
 	}
 }
