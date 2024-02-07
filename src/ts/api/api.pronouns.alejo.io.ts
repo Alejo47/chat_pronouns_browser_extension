eeed985e655615db99c9600e83ee2353ceb28289
Alejo Pereyra <alejo@alejo.io>
2024-02-07 10:17:54 -0300

    Mod: Prevent duplicated requests when fetching users' pronouns and cache locally for 15 minutes

--- src/ts/api/api.pronouns.alejo.io.ts
+++ src/ts/api/api.pronouns.alejo.io.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { GetPronounsResponseValidator } from "../types/pronouns";
-import { UserValidator } from "../types/users";
+import { User, UserValidator } from "../types/users";
+
+const userFetchingCache: Record<string, Promise<User | undefined>> = {};
 
 export const get = async <T>(
   endpoint: string,
@@ -57,7 +59,23 @@ export const getPronouns = async () => {
 };
 
 export const getUser = async (username: string) => {
-  const res = await get("/users/" + username, UserValidator);
+  if (userFetchingCache[username] !== undefined) {
+    return await userFetchingCache[username];
+  }
 
-  return res;
+  const promise = new Promise<User | undefined>(async (res, rej) => {
+    try {
+      const response = await get("/users/" + username, UserValidator);
+
+      res(response);
+    } catch (ex) {
+      rej(ex);
+    }
+  });
+
+  userFetchingCache[username] = promise;
+  setTimeout(() => {
+    delete userFetchingCache[username];
+  }, 15 * 60 * 1000);
+  return await promise;
 };
