d0b27c1c07fb1c8b37dfdf33e8c096564684d6f9
Alejo Pereyra <alejo@alejo.io>
2024-10-29 01:16:05 -0300

    Fix: Live chat selectors

--- src/ts/constants/selectors.ts
+++ src/ts/constants/selectors.ts
@@ -1,8 +1,7 @@
 export const ROOT = `#root`;
 
 export const LIVE_CHAT_DISPLAY_NAME = `span.chat-author__display-name`;
-export const LIVE_CHAT_BADGES =
-  `.chat-line__username-container > span:not(.chat-line__username)`;
+export const LIVE_CHAT_BADGES = `.chat-line__username-container > span:not(.chat-line__username)`;
 
 export const VOD_CHAT_USERNAME = `[data-a-target="chat-message-username"]`;
 export const VOD_CHAT_BADGES = `div span:has(+ a + div)`;
