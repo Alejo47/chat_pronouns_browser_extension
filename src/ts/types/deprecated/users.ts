6682e326e21b326e36401224a89ca432d77f3fed
Alejo Pereyra <alejo@alejo.io>
2024-11-18 19:09:32 -0300

    Remove: Old API files

--- src/ts/types/deprecated/users.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const UserValidator = z.object({
-  id: z.string(),
-  login: z.string(),
-  pronoun_id: z.string(),
-});
-
-export type User = z.infer<typeof UserValidator>;
